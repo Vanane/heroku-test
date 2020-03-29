4a4ecb78ec1e4fc062478476cca8d08c896e0474
Vanane <blackfox248@gmail.com>
2020-03-29 13:08:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<meta charset="UTF-8">
+<html>
+    <link rel="stylesheet" href="css.css">    
+    <script src="js.js"></script>
+    <body>
+        <div class="col left-col">
+            <div class="row"></div>
+            <div class="row"></div>
+            <div class="row"></div>
+        </div>
+        <div class="col center-col">
+            <div class="row">
+                <a id="button" class="button">Go</a>
+                <div id="names" style="overflow:visible; text-align:center"></div>
+            </div>
+        </div>
+        <div class="col right-col">
+            <div class="row"></div>
+            <div class="row"></div>
+            <div class="row"></div>
+        </div>
+    </body>
+</html>
