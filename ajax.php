d20caa87faa9c7d2c8e01030178008b8dad15ea0
Vanane <blackfox248@gmail.com>
2020-07-13 19:33:25 +0200

    Add files via upload

---
+++ ajax.php
@@ -0,0 +1,5 @@
+<?php
+require('./service.php');
+
+echo GenerateName();
+?>
--- index.php
+++ index.php
@@ -1,7 +1,10 @@
+<?php 
+echo <<<HTML
 <meta charset="UTF-8">
 <html>
     <link rel="stylesheet" href="css.css">    
     <script src="js.js"></script>
+    <service-container></service-container>
     <body>
         <div class="col left-col">
             <div class="row"></div>
@@ -11,7 +14,10 @@
         <div class="col center-col">
             <div class="row">
                 <a id="button" class="button">Go</a>
-                <div id="names" style="overflow:visible; text-align:center"></div>
+                <div id="names" style="overflow:visible; text-align:center">
+                    <table id="tabnames" style="text-align:center"></table>
+                </div>
+                
             </div>
         </div>
         <div class="col right-col">
@@ -21,3 +27,6 @@
         </div>
     </body>
 </html>
+HTML;
+?>
+
